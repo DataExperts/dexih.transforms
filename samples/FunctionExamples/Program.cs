cf5a5c6dd06513bde2b0b38ad00717564a68d5e8
Gary Holland <gary_holland@hotmail.com>
2016-06-28 22:18:22 +1000

    fixing tests

---
+++ samples/FunctionExamples/Program.cs
@@ -0,0 +1,31 @@
+﻿using dexih.functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static dexih.functions.DataType;
+
+namespace FunctionExamples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parameter parameter = new dexih.functions.Parameter("test", ETypeCode.Boolean);
+            parameter.SetValue("true");
+
+            Console.WriteLine(parameter.Value);
+
+            Function custom = new Function("CustomColumn", false, "Test", "return value1 + value2.ToString();", null, ETypeCode.String,
+                new dexih.functions.Parameter[] {
+                    new dexih.functions.Parameter("value1", ETypeCode.String, false),
+                    new dexih.functions.Parameter("value2", ETypeCode.Int32, false),
+                }, null);
+            Console.WriteLine(custom.CreateFunctionMethod().Success);
+            Console.WriteLine((string)custom.RunFunction(new object[] { "abc", "123" }).Value);
+            Console.WriteLine((bool)custom.RunFunction(new object[] { "123", "abc" }).Success);
+
+            Console.Read();
+        }
+    }
+}
