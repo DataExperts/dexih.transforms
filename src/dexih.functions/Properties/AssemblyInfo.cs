d708f44ec453f8907856122e74d86f9f9cf7b6ae
Gary holland <gholland@dataexpertsgroupcom>
2016-11-18 11:33:08 +1100

    fixed project.json files

--- src/dexih.functions/Properties/AssemblyInfo.cs
+++ src/dexih.functions/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("373d8f41-c08f-4512-a8ef-3fdcae6a27f9")]
+//[assembly: Guid("373d8f41-c08f-4512-a8ef-3fdcae6a27f9")]
