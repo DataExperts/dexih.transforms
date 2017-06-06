9a646270d5fd2907e6d912e54f81674aa6277fb3
Gary Holland <gholland@dataexpertsgroup.com>
2017-06-06 13:55:05 +1000

    fixing tests

---
+++ src/dexih.connections.flatfile/FlatFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace dexih.functions
+{
+	public class File : Table
+	{
+		public bool UseCustomFilePaths { get; set; }
+		public string FileRootPath { get; set; }
+		public string FileIncomingPath { get; set; }
+		public string FileProcessedPath { get; set; }
+		public string FileRejectedPath { get; set; }
+		public string FileMatchPattern { get; set; }
+
+		public File()
+		{
+		}
+	}
+}
---
+++ src/dexih.connections.webservice.restful/RestFunction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using dexih.functions;
+
+namespace dexih.connections.webservice.restful
+{
+	public class WebAPI : Table
+	{
+		public string RestfulUri { get; set; }
+		public string RowPath { get; set; }
+
+		public WebAPI()
+		{
+		}
+	}
+}
