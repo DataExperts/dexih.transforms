85f5b19b1c1613e35449d446aead3f0ed046c5de
Gary Holland <gholland@dataexpertsgroup.com>
2017-08-03 20:56:26 +1000

    appveyor updates

---
+++ build_packages.sh
@@ -0,0 +1,11 @@
+dotnet pack ./src/dexih.functions -o ../../artifacts
+dotnet pack ./src/dexih.transforms -o ../../artifacts
+dotnet pack ./src/dexih.connections.azure -o ../../artifacts
+dotnet pack ./src/dexih.connections.dexih -o ../../artifacts
+dotnet pack ./src/dexih.connections.excel -o ../../artifacts
+dotnet pack ./src/dexih.connections.flatfile -o ../../artifacts
+dotnet pack ./src/dexih.connections.sql -o ../../artifacts
+dotnet pack ./src/dexih.connections.webservice.restful -o ../../artifacts
+
+
+
