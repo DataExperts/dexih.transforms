d49a1b49e3d945b1af3df7868aaa21f753e415fc
Gary Holland <gholland@dataexpertsgroup.com>
2017-08-21 12:21:56 +1000

    Managed Tasks update

---
+++ src/dexih.functions/ManagedTasks/ManagedTask.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using dexih.functions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace dexih.functions
+{
+    public enum EManagedTask
+    {
+        Initialized, Started, Running, Success, Error
+    }
+    
+    public class ManagedTask: ReturnValue, IDisposable
+    {
+        public delegate void Completed(string reference);
+        public event Completed OnCompleted;
+
+        public delegate void Progress(ManagedTask managedTask);
+        public event Progress OnProgress;
+
+        /// <summary>
+        /// Unique key used to reference the task
+        /// </summary>
+        public string Reference { get; set; }
+        
+        /// <summary>
+        /// Id that reference the originating client of the task.
+        /// </summary>
+        public string OriginatorId { get; set; }
+        
+        /// <summary>
+        /// Short description for the task.
+        /// </summary>
+        public string Title { get; set; }
+        
+        /// <summary>
+        /// When task was last updated.
+        /// </summary>
+        public DateTime LastUpdate { get; set; }
+
+        public int Percentage { get; set; }
+
+        /// <summary>
+        /// Action that will be started and executed with the task.
+        /// </summary>
+        [JsonIgnore]
+        public Action<IProgress<int>, CancellationToken> Action { get; set; }
+
+        private CancellationTokenSource _cancellationTokenSource;
+        
+        private Task _task;
+        private readonly IProgress<int> _progress;
+
+
+        public ManagedTask()
+        {
+            var progressHandler = new Progress<int>(value =>
+            {
+                Percentage = value;
+                OnProgress?.Invoke(this);
+            });
+
+            _progress = progressHandler;
+        }
+
+        public void Start()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            _task = Task.Run(() => Action.Invoke(_progress, _cancellationTokenSource.Token), _cancellationTokenSource.Token);
+
+            // when task finishes, raise an oncompleted event.
+            _task.ContinueWith((task) => {
+                switch (task.Status)
+                {
+                    case TaskStatus.Canceled:
+                        Success = false;
+                        Message = "The task was cancelled.";
+                        return;
+                    case TaskStatus.Created:
+                        break;
+                    case TaskStatus.Faulted:
+                        Success = false;
+                        Message = task.Exception?.Message;
+                        Exception = task.Exception;
+                        return;
+                    case TaskStatus.RanToCompletion:
+                        Success = true;
+                        Message = "The task completed successfully.";
+                        return;
+                    case TaskStatus.Running:
+                    case TaskStatus.WaitingForActivation:
+                    case TaskStatus.WaitingForChildrenToComplete:
+                    case TaskStatus.WaitingToRun:
+                        Success = false;
+                        Message = "Unexpected task status when should be completed: " + task.Status.ToString();
+                        return;
+                }
+                OnCompleted?.Invoke(Reference);
+                this.Percentage = 100;
+                OnProgress?.Invoke(this);
+            });
+        }
+
+        public  void Cancel()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        public void Dispose()
+        {
+            OnCompleted = null;
+            _cancellationTokenSource.Dispose();
+        }
+    }
+}
---
+++ src/dexih.functions/ManagedTasks/ManagedTasks.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dexih.functions
+{
+    /// <summary>
+    /// Simple collection of managed tasks
+    /// </summary>
+    public class ManagedTasks : IEnumerable<ManagedTask>
+    {
+        public delegate void Completed(ManagedTask managedTask);
+        public event Completed OnCompleted;
+
+		public delegate void Progress(ManagedTask managedTask);
+		public event Progress OnProgress;
+
+		private readonly ConcurrentDictionary<string, ManagedTask> _managedTasks;
+
+        private AsyncAutoResetEvent _resetWhenNoTasks; //event handler that triggers when all tasks completed.
+
+        public ManagedTasks()
+        {
+            _managedTasks = new ConcurrentDictionary<string, ManagedTask>();
+            _resetWhenNoTasks = new AsyncAutoResetEvent();
+        }
+    
+        public IEnumerator<ManagedTask> GetEnumerator()
+        {
+            return _managedTasks.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
+        /// <summary>
+        /// Creates & starts a new managed task.
+        /// </summary>
+        /// <param name="originatorId">Id that can be used to referernce where the task was started from.</param>
+        /// <param name="title">Short description of the task.</param>
+        /// <param name="action">The action </param>
+        /// <returns></returns>
+        public string Add(string originatorId, string title, Action<IProgress<int>, CancellationToken> action)
+        {
+            
+            var reference = Guid.NewGuid().ToString();
+
+            var managedTask = new ManagedTask()
+            {
+                Reference = reference,
+                OriginatorId = originatorId,
+                Title =  title,
+                Action = action,
+                LastUpdate = DateTime.Now
+            };
+            
+            managedTask.OnCompleted += ManagedTaskCompleted;
+            managedTask.Start();
+
+            _managedTasks.TryAdd(reference, managedTask);
+
+            return reference;
+        }
+
+        public void ManagedTaskCompleted(string reference)
+        {
+            ManagedTask managedTask;
+            _managedTasks.TryRemove(reference, out managedTask);
+            managedTask.Dispose();
+            OnCompleted?.Invoke(managedTask);
+        }
+
+        public void ManagedTaskProgress(ManagedTask managedTask)
+        {
+            OnProgress?.Invoke(managedTask);
+        }
+
+        public async Task WhenAll()
+        {
+            if (_managedTasks.Count > 0)
+            {
+                await _resetWhenNoTasks.WaitAsync();
+            }
+        }
+    }
+}
