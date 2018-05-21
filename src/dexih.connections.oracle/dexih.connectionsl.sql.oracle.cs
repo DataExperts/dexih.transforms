9f33b7e1e03d07c83ee24091c9db38c580aeba8c
Gary Holland <gholland@dataexpertsgroup.com>
2018-05-21 10:34:15 +1000

    fix reference

--- src/dexih.connections.oracle/dexih.connectionsl.sql.oracle.cs
+++ src/dexih.connections.oracle/dexih.connectionsl.sql.oracle.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading;
