0a42ad6309e28a93f902bffa4af134ad2509147f
Gary Holland <gholland@dataexpertsgroup.com>
2019-08-29 15:21:14 +1000

    refacting

---
+++ src/dexih.transforms/StreamAsyncAction.cs
@@ -0,0 +1,88 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Dexih.Utils.Crypto;
+using Newtonsoft.Json.Linq;
+
+namespace dexih.transforms
+{
+    public class StreamAction<T>: Stream
+    {
+        private readonly MemoryStream _memoryStream;
+        private readonly StreamWriter _streamWriter;
+        private long _position;
+        
+        private readonly Func<T> _func;
+        private bool isFirst = true;
+
+        public StreamAction(Func<T> func)
+        {
+            _func = func;
+            _memoryStream = new MemoryStream();
+            _streamWriter = new StreamWriter(_memoryStream) {AutoFlush = true};
+            _position = 0;
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length => -1;
+
+        public override long Position { get => _position; set => throw new NotSupportedException("The position cannot be set."); }
+
+        public override void Flush()
+        {
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (isFirst)
+            {
+                var result = _func.Invoke();
+                var json = Json.SerializeObject(result, "");
+                _streamWriter.Write(json);
+                _memoryStream.Position = 0;
+                isFirst = false;
+            }
+
+            var readCount = _memoryStream.Read(buffer, offset, count);
+            return readCount;        
+        }
+
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken = default)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                throw new OperationCanceledException();
+            }
+            var position = Read(buffer, offset, count);
+            return Task.FromResult(position);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException("The Seek function is not supported.");
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException("The SetLength function is not supported.");
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException("The Write function is not supported.");
+        }
+        
+        public override void Close()
+        {
+            _streamWriter?.Close();
+            _memoryStream?.Close();
+            base.Close();
+        }
+    }
+}
