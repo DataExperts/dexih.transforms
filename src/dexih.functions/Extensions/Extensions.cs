512711e866e57c27c84d09177bbb3d3a4a1c29bc
Gary Holland <gholland@dataexpertsgroup.com>
2019-10-07 10:44:07 +1100

    appveyor update

---
+++ src/dexih.functions/Extensions/Extensions.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace dexih.functions
+{
+    public static class Extensions
+    {
+        
+        /// <summary>
+        /// Use to get a value or a default from a dictionary.
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <typeparam name="TK"></typeparam>
+        /// <typeparam name="TV"></typeparam>
+        /// <returns></returns>
+        public static TV GetValueOrDefault<TK, TV>(this IDictionary<TK, TV> dict, TK key, TV defaultValue = default(TV))
+        {
+            return dict.TryGetValue(key, out var value) ? value : defaultValue;
+        }
+
+        /// <summary>
+        /// Checks if an object is null or a blank string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool ObjectIsNullOrBlank(this object value)
+        {
+            if (value is null)
+            {
+                return true;
+            }
+
+            if (value is string s && string.IsNullOrWhiteSpace(s))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        
+        public static bool IsPattern(this string value, string pattern)
+        {
+            if (value.Length != pattern.Length) return false;
+            for (var i = 0; i < pattern.Length; i++)
+            {
+                if ((pattern[i] == '9' && !char.IsNumber(value[i])) ||
+                    (pattern[i] == 'A' && !char.IsUpper(value[i])) ||
+                    (pattern[i] == 'a' && !char.IsLower(value[i])) ||
+                    (pattern[i] == 'Z' && !char.IsLetter(value[i])))
+                    return false;
+            }
+
+            return true;
+        }
+        
+        public static string CreateSHA1(this string value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            
+            var bytes = Encoding.UTF8.GetBytes(value);
+            using (var sha1 = System.Security.Cryptography.SHA1.Create())
+            {
+                var hash = sha1.ComputeHash(bytes);
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                var sBuilder = new StringBuilder();
+
+                for (var i = 0; i < hash.Length; i++)
+                {
+                    sBuilder.Append(hash[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+        
+        public static DateTime UnixTimeStampToDate(this long unixTimeStamp)
+        {
+            var origDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var convertedDate = origDate.AddSeconds(unixTimeStamp).ToLocalTime();
+            return convertedDate;
+        }
+
+        public static T ToObject<T>(this object value)
+        {
+            if (value is T tValue)
+            {
+                return tValue;
+            }
+
+            if (value is JsonDocument jsonDocument)
+            {
+                return jsonDocument.ToObject<T>();
+            }
+
+            if (value is JsonElement jsonElement)
+            {
+                return jsonElement.ToObject<T>();
+            }
+            
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
---
+++ src/dexih.functions/Extensions/JsonExtensions.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dexih.functions
+{
+    public static class JsonExtensions
+    {
+        private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase, 
+//            IgnoreNullValues = true
+        };
+        
+        public static T ToObject<T>(this JsonElement element)
+        {
+            var json = element.GetRawText();
+            return JsonSerializer.Deserialize<T>(json, SerializerOptions);
+        }
+
+        public static T ToObject<T>(this JsonDocument document)
+        {
+            var json = document.RootElement.GetRawText();
+            return JsonSerializer.Deserialize<T>(json, SerializerOptions);
+        }
+        
+        public static string Serialize<T>(this T value)
+        {
+            return JsonSerializer.Serialize(value, SerializerOptions);
+        }
+
+        public static T Deserialize<T>(this string value)
+        {
+            return JsonSerializer.Deserialize<T>(value, SerializerOptions);
+        }
+
+        public static object Deserialize(this string value, Type type)
+        {
+            return JsonSerializer.Deserialize(value, type, SerializerOptions);
+        }
+
+        public static JsonDocument ToJsonDocument(this object content)
+        {
+            byte[] utf8JsonData = JsonSerializer.SerializeToUtf8Bytes(content);
+            return JsonDocument.Parse(utf8JsonData);
+        }
+
+        public static Task SerializeAsync<T>(this Stream stream, T value, CancellationToken cancellationToken)
+        {
+            return JsonSerializer.SerializeAsync(stream, value, SerializerOptions, cancellationToken);
+        }
+
+        public static ValueTask<T> DeserializeAsync<T>(this Stream stream, CancellationToken cancellationToken)
+        {
+            return JsonSerializer.DeserializeAsync<T>(stream, SerializerOptions, cancellationToken);
+        }
+    }
+}
